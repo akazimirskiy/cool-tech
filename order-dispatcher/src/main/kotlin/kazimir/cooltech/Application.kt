ae020e5145cea90a0afa9202dd78755b6f7b888e
kazimir <alexander.kazimirsky@billingplatform.com>
2024-12-29 17:07:17 -0300

    Reorganized proto

---
+++ order-dispatcher/src/main/kotlin/kazimir/cooltech/Application.kt
@@ -0,0 +1,8 @@
+package kazimir.cooltech
+
+import io.micronaut.runtime.Micronaut.run
+
+fun main(args: Array<String>) {
+    run(*args)
+}
+
---
+++ payments-api/src/test/kotlin/kazimir/cooltech/paymentsapi/PaymentsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package kazimir.cooltech.paymentsapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentsApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
